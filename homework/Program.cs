16b40d030f3952165fd518cc982364369b11b09c
699evkp <tu699evkp@code.edu.az>
2022-03-14 13:44:01 +0400

    homework

---
+++ homework/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle mashin = new Vehicle();
+
+            mashin.color = "qara";
+            mashin.year = 2019;
+            
+
+            Console.WriteLine(mashin.color);
+            Console.WriteLine(mashin.year);
+        }
+        
+        
+
+    }
+}
---
+++ homework/Vehicle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework
+{
+    class Vehicle
+    {
+        public string color;
+        public int year;
+        public string brand;
+        public string model;
+        public int FuelCapacity;
+        public int FuelFor1Km;
+        public int CurrentFuel;
+
+        public Vehicle()
+        {
+
+        }
+
+        public Vehicle(string color)
+
+        {
+            this.color = color;
+        }
+        public Vehicle(string color, string model) : this(color)
+        {
+            this.model = model;
+        }
+        public Vehicle(string brand, string color, string model) : this(color, model)
+        {
+            this.brand = brand;
+        }
+        public Vehicle(string brand, string color, string model, int year) : this(color, model, brand)
+        {
+            this.year = year;
+        }
+        public Vehicle(string brand, string color, string model, int year, int FuelCapacity) : this(color, model, brand, year)
+        {
+            this.FuelCapacity = FuelCapacity;
+        }
+        public Vehicle(string brand, string color, string model, int year, int FuelCapacity, int FuelFor1km) : this(color, model, brand, year, FuelCapacity)
+
+        {
+            this.FuelFor1Km = FuelFor1Km;
+        }
+
+
+        public Vehicle(string brand, string color, string model, int year, int FuelCapacity, int FuelFor1km, int CurrentFuel) : this(color, model, brand, year, FuelCapacity, FuelFor1Km)
+        {
+            this.CurrentFuel = CurrentFuel;
+        }
+
+
+    }
+    
